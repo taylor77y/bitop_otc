bfca53566f68e79db7af03c209e2e9f1d3ab1728
taylor <taylor77y@gmail.com>
2022-01-08 23:13:11 -0800

    新增取消申诉方法

--- src/main/java/com/bitop/otcapi/fcg/service/impl/OtcOrderAppealServiceImpl.java
+++ src/main/java/com/bitop/otcapi/fcg/service/impl/OtcOrderAppealServiceImpl.java
@@ -1,8 +1,10 @@
 package com.bitop.otcapi.fcg.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.bitop.otcapi.constant.MatchOrderStatus;
+import com.bitop.otcapi.constant.SysOrderConstants;
 import com.bitop.otcapi.constant.SysTipsConstants;
 import com.bitop.otcapi.context.ContextHandler;
 import com.bitop.otcapi.fcg.entity.OtcBankCard;
@@ -73,13 +75,52 @@ public class OtcOrderAppealServiceImpl extends ServiceImpl<OtcOrderAppealMapper,
         AsyncManager.me().execute(AsyncFactory.StationLetter(toUserId,
                 SysTipsConstants.TipsType.APPEALING, orderMatch.getOrderNo(), toName));
         //给于对方和自己信号（otc聊天信息表）
-//        AsyncManager.me().execute(AsyncFactory.sendSysChat(userId, toUserId, orderMatch.getOrderMatchNo(),
-//                SysOrderConstants.SysChatMsg.APPEALING, MatchOrderStatus.APPEALING));
+        AsyncManager.me().execute(AsyncFactory.sendSysChat(userId, toUserId, orderMatch.getOrderMatchNo(),
+                SysOrderConstants.SysChatMsg.APPEALING, MatchOrderStatus.APPEALING));
         return Response.success();
     }
 
+    /**
+     * 取消申诉
+     *
+     * @param id
+     * @return
+     */
     @Override
+    @Transactional(isolation = Isolation.REPEATABLE_READ, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)//value="transactionManager1"
     public Response cancelAppeal(String id) {
-        return null;
+        String userId=ContextHandler.getUserId();
+        //通过id查询到申诉信息
+        OtcOrderAppeal ezOtcOrderAppeal = baseMapper.selectById(id);
+        if (!"1".equals(ezOtcOrderAppeal.getStatus())) {
+            return Response.error(MessageUtils.message("申诉状态已改变"));
+        }
+        String orderMatchNo = ezOtcOrderAppeal.getOrderMatchNo();
+        //查看订单的申诉
+        OtcOrderMatch orderMatch = matchService.getById(orderMatchNo);
+        if (!orderMatch.getStatus().equals(MatchOrderStatus.APPEALING.getCode())){
+            return Response.error(MessageUtils.message("订单状态已发生变化"));
+        }
+        LambdaQueryWrapper<OtcOrderAppeal> queryWrapper = Wrappers.<OtcOrderAppeal>lambdaQuery()
+                .eq(OtcOrderAppeal::getOrderMatchNo, orderMatchNo)
+                .and(wq -> wq.eq(OtcOrderAppeal::getStatus, "1")
+                        .or().eq(OtcOrderAppeal::getStatus, "3")
+                        .or().eq(OtcOrderAppeal::getStatus, "4"));
+        Integer integer = baseMapper.selectCount(queryWrapper);
+        if (integer == 1) {
+            orderMatch.setStatus(MatchOrderStatus.PAID.getCode());
+            matchService.updateById(orderMatch);
+            String toUserId = userId.equals(orderMatch.getUserId()) ? orderMatch.getOtcOrderUserId() : orderMatch.getUserId();
+            String toName = userId.equals(orderMatch.getUserId()) ? orderMatch.getAdvertisingName() : orderMatch.getMatchAdvertisingName();
+
+            AsyncManager.me().execute(AsyncFactory.StationLetter(toUserId,
+                    SysTipsConstants.TipsType.APPEAL_OFF, orderMatch.getOrderNo(), toName));
+
+            AsyncManager.me().execute(AsyncFactory.sendSysChat(orderMatch.getUserId(), orderMatch.getOtcOrderUserId(), orderMatchNo,
+                    SysOrderConstants.SysChatMsg.APPEAL_OFF, MatchOrderStatus.PAID));
+        }
+        ezOtcOrderAppeal.setStatus("2");
+        baseMapper.updateById(ezOtcOrderAppeal);
+        return Response.success();
     }
 }
